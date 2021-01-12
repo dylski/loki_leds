1b313c4b2ca6928ee5ce9935ae4495f26b1470e8
Dylan Banarse <dylan@banarse.com>
2021-01-12 00:15:44 +0000

    Fix for Loki code restructure

--- loki_leds.py
+++ loki_leds.py
@@ -1,8 +1,7 @@
-#from loki.loki1Dv import *
-#from midivis.mapped_fairy_lights import *
-
+# Run Loki sim and display on LED strings
 import cv2
-import loki.loki1Dv as loki
+from loki.loki1Dv import Loki
+from loki.main import get_config
 import midivis.mapped_fairy_lights as mfl
 import numpy as np
 
@@ -10,10 +9,7 @@ if __name__ == '__main__':
 
   # mfl.bouncer(colours=((255,10,3),(10,255,23)))
 
-  # config = loki.get_config(show_lock=True,
-  #         extraction_method='mean', width=128, num_1d_history=240,
-  #         display='headless', render_texture='energy_up')
-  config = loki.get_config(width=25,
+  config = get_config(width=25,
       height=None,
       render_colour='rgb',
       # render_texture='energy_down',  # 'flat', 'energy_up', 'energy_down'
@@ -37,27 +33,31 @@ if __name__ == '__main__':
       # extraction_rate=0.01,
       # lock_mutation=0.001,
 
+      key_mean_mutation=0.1,
+      key_sigma_mutation=0.1,
       extraction_method='mean',
       show_lock=False,
-      # display='headless',
       save_frames=False,
       log_data=None,
+      landscape_roughness=1,
       landscape='wobbly')
 
-  sim = loki.Loki(config)
-  leds = mfl.MappedFairyLights(num_lights=150,
-      calib_file='calib/bedroom.npy')
-      # calib_file='office_vertical_calib.npy')
+  show_leds = True
+
+  sim = Loki(config)
+  if show_leds:
+    leds = mfl.MappedFairyLights(num_lights=150,
+        calib_file='calib/bedroom.npy')
+        # calib_file='office_vertical_calib.npy')
 
   while(True):
     frame = sim.step_frame()
-    # print(np.array(frame).mean())
-    # print(sim._bitmap.mean())
-    bitmap = sim._bitmap
-    # print(bitmap.shape)
-    bitmap = bitmap[:, 8:, :]
-    #bitmap = cv2.transpose(bitmap)
-    #bitmap = cv2.flip(bitmap, 0)
-    #bitmap = cv2.transpose(bitmap)
-    #bitmap = cv2.transpose(bitmap)
-    leds.show_image2(bitmap, format='rgb')
+    if show_leds:
+      bitmap = sim._bitmap
+      # print(bitmap.shape)
+      bitmap = bitmap[:, 8:, :]
+      #bitmap = cv2.transpose(bitmap)
+      #bitmap = cv2.flip(bitmap, 0)
+      #bitmap = cv2.transpose(bitmap)
+      #bitmap = cv2.transpose(bitmap)
+      leds.show_image2(bitmap, format='rgb')
